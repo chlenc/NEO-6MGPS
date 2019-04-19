7facbaf5e45678254e7b59d2b4a79e3154ceff9a
Алешенька:3 <alexnagorny.an@gmail.com>
2019-04-19 19:57:49 +0300

    add button

--- NEO-6MGPS.ino
+++ NEO-6MGPS.ino
@@ -1,8 +1,8 @@
 /*********************
- *10 to GPS Module TX*
- *09 to GPS Module RX*
+  10 to GPS Module TX
+  09 to GPS Module RX
  *********************/
-
+int button = 2;
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
 
@@ -14,32 +14,32 @@ void printFloat(double f, int digits = 2);
 
 uint32_t counter = 0;
 
-void setup()  
+void setup()
 {
   Serial.begin(9600);
   mySerial.begin(9600);
   delay(1000);
 }
 
-void loop() 
+void loop()
 {
   bool newdata = false;
   unsigned long start = millis();
   // Every 5 seconds we print an update
-  while (millis() - start < 5000) 
+  while (millis() - start < 5000)
   {
-    if (mySerial.available()) 
-    
+    if (mySerial.available())
+
     {
       char c = mySerial.read();
-      if (gps.encode(c)) 
+      if (gps.encode(c))
       {
         newdata = true;
-        break;  
+        break;
       }
     }
   }
-  
+
   if (newdata) {
     gpsdump(gps);
   }
@@ -53,29 +53,30 @@ void gpsdump(TinyGPS &gps)
   int year;
   byte month, day, hour, minute, second, hundredths;
   unsigned short sentences, failed;
-
-  gps.f_get_position(&flat, &flon, &age);
-  Serial.print(counter++); 
-  Serial.print(": "); 
-  printFloat(flat, 5); 
-  Serial.print(", ");
-  printFloat(flon, 5);
-  Serial.println();
+  if (digitalRead(button) == HIGH) {
+    gps.f_get_position(&flat, &flon, &age);
+    Serial.print(counter++);
+    Serial.print(": ");
+    printFloat(flat, 5);
+    Serial.print(", ");
+    printFloat(flon, 5);
+    Serial.println();
+  }
 }
 
 void printFloat(double number, int digits)
 {
   // Handle negative numbers
-  if (number < 0.0) 
+  if (number < 0.0)
   {
-     Serial.print('-');
-     number = -number;
+    Serial.print('-');
+    number = -number;
   }
 
   double rounding = 0.5;
-  for (uint8_t i=0; i<digits; ++i)
+  for (uint8_t i = 0; i < digits; ++i)
     rounding /= 10.0;
-  
+
   number += rounding;
 
   // Extract the integer part of the number and print it
@@ -85,10 +86,10 @@ void printFloat(double number, int digits)
 
   // Print the decimal point, but only if there are digits beyond
   if (digits > 0)
-    Serial.print("."); 
+    Serial.print(".");
 
   // Extract digits from the remainder one at a time
-  while (digits-- > 0) 
+  while (digits-- > 0)
   {
     remainder *= 10.0;
     int toPrint = int(remainder);
